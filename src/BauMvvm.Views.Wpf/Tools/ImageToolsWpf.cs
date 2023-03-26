75378608abb7d3f875fabaf24961677e92599a50
Jose Antonio Bautista Montejo <jbautistam@gmail.com>
2023-03-26 19:01:57 +0200

    Explorer loading

---
+++ src/BauMvvm.Views.Wpf/Tools/ImageToolsWpf.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Bau.Libraries.BauMvvm.Views.Wpf.Tools;
+
+/// <summary>
+///		Herramientas para tratamiento de imágenes en WPF
+/// </summary>
+public class ImageToolsWpf
+{
+	/// <summary>
+	///		Crea la imagen en memoria (útil para cargar una imagen desde un archivo y que no se bloquee el acceso)
+	/// </summary>
+	public ImageSource CreateBitmapImage(string fileName)
+	{
+		BitmapImage image = new BitmapImage();
+
+			// Lee el archivo sobre la imagen
+			image.BeginInit();
+			image.StreamSource = new System.IO.FileStream(fileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+			image.CacheOption = BitmapCacheOption.OnLoad;
+			image.EndInit();
+			// Libera el stream para evitar excepciones de acceso al archivo cuando se intenta borrar la imagen
+			image.StreamSource.Dispose();
+			// Asigna la imagen
+			return image;
+	}
+}
