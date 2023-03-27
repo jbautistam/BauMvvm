9eb1554d5ff5a9551fe9a8af2dfdcf129e1c6682
Jose Antonio Bautista Montejo <jbautistam@gmail.com>
2023-03-27 11:45:09 +0200

    Control memoria en los árboles del explorador

--- src/BauMvvm.Views.Wpf/Tools/ImageToolsWpf.cs
+++ src/BauMvvm.Views.Wpf/Tools/ImageToolsWpf.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Media;
+﻿using System;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Bau.Libraries.BauMvvm.Views.Wpf.Tools;
@@ -11,7 +12,7 @@ public class ImageToolsWpf
 	/// <summary>
 	///		Crea la imagen en memoria (útil para cargar una imagen desde un archivo y que no se bloquee el acceso)
 	/// </summary>
-	public ImageSource CreateBitmapImage(string fileName)
+	public ImageSource GetFromFileName(string fileName)
 	{
 		BitmapImage image = new BitmapImage();
 
@@ -24,5 +25,14 @@ public class ImageToolsWpf
 			image.StreamSource.Dispose();
 			// Asigna la imagen
 			return image;
+	}
+
+	/// <summary>
+	///		Obtiene una imagen a partir de un Uri
+	/// </summary>
+	/// <param name="uri">La cadena Uri debe ser del tipo "pack://application:,,,/BauControls;component/Themes/Images/Solution.png"</param>
+	public ImageSource GetFromResource(string uri)
+	{
+		return new BitmapImage(new Uri(uri));
 	}
 }
